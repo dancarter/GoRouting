43008981820b20e1dc06540ae8da6c7dfd456138
Dan Carter <dcarter@adharmonics.com>
2015-01-08 20:59:34 -0500

    Add simple route with test

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "net/http"
+import "fmt"
 
+func NewMux() *http.ServeMux {
+  mux := http.NewServeMux()
+  users := func(res http.ResponseWriter, req *http.Request) {
+    fmt.Fprint(res, "Users Index")
+  }
+  mux.HandleFunc("/users", users)
+  return mux
+}
+
+func main() {
+  http.ListenAndServe(":3000", NewMux())
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+  "io/ioutil"
+  "net/http"
+  "net/http/httptest"
   "testing"
+
   "github.com/stretchr/testify/assert"
 )
 
 
 func Test_Routing(t *testing.T) {
-  a := seert.New(t)
+  a := assert.New(t)
+
+  ts := httptest.NewServer(NewMux())
+  defer ts.Close()
+
+  res, err := http.Get(ts.URL + "/users")
+  a.NoError(err)
+  body, err := ioutil.ReadAll(res.Body)
+  a.Equal(string(body), "Users Index")
 }
